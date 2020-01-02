5d2fbe5e6505d433e5c1b8e2ed247271d93e9dda
vinic-asouza <vinic.asouza@gmail.com>
2020-01-02 15:14:55 -0300

    connect api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,58 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, { Component } from 'react';
+import {Keyboard} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+import {Container, Form, Input, SubmitButton} from './styles';
 
-export default function Main() {
-  return <View />;
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleAddUser = async () => {
+    const { newUser, users } = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
+  }
+
+  render() {
+    const { newUser, users } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar Usuário"
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#FFF" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 Main.navigationOptions = {
